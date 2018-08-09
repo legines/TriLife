ec78eb1af9876afc5e533cf26e44aa6f70ee3496
curl http://installrails.com/update_rubygems.rb | ruby <legines@gmail.com>
2018-08-09 00:47:51 -0400

    register styling

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import BoxContainer from "../../components/BoxContainer";
+import { Container } from "../../components/Grid";
 import './Login.css';
 
 class Create extends Component {
@@ -31,16 +33,21 @@ class Create extends Component {
   render() {
     const { username, password } = this.state;
     return (
-      <div class="container">
-        <form class="form-signin" onSubmit={this.onSubmit}>
-          <h2 class="form-signin-heading">Register</h2>
-          <label for="inputEmail" class="sr-only">Email address</label>
-          <input type="email" class="form-control" placeholder="Email address" name="username" value={username} onChange={this.onChange} required/>
-          <label for="inputPassword" class="sr-only">Password</label>
-          <input type="password" class="form-control" placeholder="Password" name="password" value={password} onChange={this.onChange} required/>
-          <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-        </form>
-      </div>
+      <Container fluid>
+      <BoxContainer>
+        <div class="container login_screen">
+          <form class="form-signin" onSubmit={this.onSubmit}>
+            <h2 class="form-signin-heading">Register</h2>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" class="form-control" placeholder="Email address" name="username" value={username} onChange={this.onChange} required/>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" class="form-control" placeholder="Password" name="password" value={password} onChange={this.onChange} required/>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+          </form>
+        </div>
+      </BoxContainer>  
+      </Container>
+        
     );
   }
 }
